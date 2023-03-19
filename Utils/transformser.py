5375fe2fad11cf6afa1b6b22c3f57fcd7592d6bb
DeepSota <91316374+DeepSota@users.noreply.github.com>
2023-03-19 22:18:41 +0800

    Add files via upload

---
+++ Utils/transformser.py
@@ -0,0 +1,185 @@
+import math
+import torch
+import random
+import numbers
+
+import numpy as np
+
+
+try:
+    import accimage
+except ImportError:
+    accimage = None
+
+
+# 将一系列转换函数集成在一起
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        for t in self.transforms:
+            img = t(img)
+        return img
+
+    def randomize_parameters(self):
+        for t in self.transforms:
+            t.randomize_parameters()
+
+
+# 将图片转换为tensor
+class ToTensor(object):
+
+    def __init__(self, norm_value=255):
+        self.norm_value = norm_value
+
+    def __call__(self, pic):
+        """
+        Args:
+            pic (PIL.Image or numpy.ndarray): Image to be converted to tensor.
+        Returns:
+            Tensor: Converted image.
+        """
+
+        if isinstance(pic, np.ndarray):
+            # handle numpy array
+            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            # backward compatibility
+            return img.float().div(self.norm_value)
+
+        if accimage is not None and isinstance(pic, accimage.Image):
+            nppic = np.zeros(
+                [pic.channels, pic.height, pic.width], dtype=np.float32)
+            pic.copyto(nppic)
+            return torch.from_numpy(nppic)
+
+        # handle PIL Image
+        if pic.mode == 'I':
+            img = torch.from_numpy(np.array(pic, np.int32, copy=False))
+        elif pic.mode == 'I;16':
+            img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+        else:
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+        # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+        if pic.mode == 'YCbCr':
+            nchannel = 3
+        elif pic.mode == 'I;16':
+            nchannel = 1
+        else:
+            nchannel = len(pic.mode)
+        img = img.view(pic.size[1], pic.size[0], nchannel)
+        # put it from HWC to CHW format
+        # yikes, this transpose takes 80% of the loading time/CPU
+        img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        if isinstance(img, torch.ByteTensor):
+            return img.float().div(self.norm_value)
+        else:
+            return img
+
+    def randomize_parameters(self):
+        pass
+
+
+# 标准化处理
+class Normalize(object):
+
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        # TODO: make efficient
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.sub_(m).div_(s)
+        return tensor
+
+    def randomize_parameters(self):
+        pass
+
+
+# 中心裁剪
+class CenterCrop(object):
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL.Image): Image to be cropped.
+        Returns:
+            PIL.Image: Cropped image.
+        """
+        w, h = img.size
+        th, tw = self.size
+        x1 = int(round((w - tw) / 2.))
+        y1 = int(round((h - th) / 2.))
+        return img.crop((x1, y1, x1 + tw, y1 + th))  # 在中心裁剪出长宽为 th，tw 的图片
+
+    def randomize_parameters(self):
+        pass
+
+    
+class target_Compose(object):
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, target):
+        dst = []
+        for t in self.transforms:
+            dst.append(t(target))
+        return dst
+    
+
+
+    
+    
+# 获取类别
+class ClassLabel(object):
+
+    def __call__(self, target):
+        return target['label']
+
+    
+class VideoID(object):
+
+    def __call__(self, target):
+        return target['video_id']
+
+
+# 从中心截取指定长度的视频
+class TemporalCenterCrop(object):
+
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, frame_indices):
+        """
+        Args:
+            frame_indices (list): frame indices to be cropped.
+        Returns:
+            list: Cropped frame indices.
+        """
+
+        center_index = len(frame_indices) // 2
+        begin_index = max(0, center_index - (self.size // 2))
+        end_index = min(begin_index + self.size, len(frame_indices))
+
+        out = frame_indices[begin_index:end_index]
+
+        for index in out:
+            if len(out) >= self.size:
+                break
+            out.append(index)
+
+        return out
---
+++ Utils/utils.py
@@ -0,0 +1,199 @@
+import os
+import sys
+import torch
+import pickle
+import random
+import pandas as pd
+import torch.nn as nn
+from mmcv import Config
+import numpy as np
+from Utils.transformser import ClassLabel,target_Compose,VideoID
+
+from datasets.c3d_dataset.dataset_c3d import get_test_set
+from mmaction.apis import init_recognizer
+
+target_transform = target_Compose([VideoID(), ClassLabel()])
+
+# 获取每个视频的总帧数
+def load_value_file(file_path):
+    with open(file_path, 'r') as input_file:
+        value = float(input_file.read().rstrip('\n\r'))
+    return value
+
+
+# 将图片转换到分类器可以使用的向量
+def image_to_vector(model_name, dataset_name, x):
+    # convert (0-255) image to specify range
+    if model_name == 'c3d':
+        if dataset_name=='k400':
+            means = torch.tensor([114.7748, 107.7354, 99.4750], dtype=torch.float32)[:, None, None, None]
+            x.add_(means)
+            x[x > 255] = 255
+            x[x < 0] = 0
+            x = x / 255
+            x = x.permute(1, 0, 2, 3)  # 帧数，通道，长宽
+        else:
+            means = torch.tensor([101.2198, 97.5751, 89.5303], dtype=torch.float32)[:, None, None, None]
+            x.add_(means)
+            x[x > 255] = 255
+            x[x < 0] = 0
+            x= x/255
+            x=x.permute(1, 0, 2 , 3)   # 帧数，通道，长宽
+    else:
+        x=x
+    return x
+
+def vector_to_image(model_name,x):
+    if model_name == 'c3d':
+        means = torch.tensor([101.2198, 97.5751, 89.5303], dtype=torch.float32)[:, None, None, None].cuda()
+        x[x > 1] = 1
+        x[x < 0] = 0
+        x=x*255
+        x.sub_(means)
+    return x
+
+
+
+
+# 获取数据集
+def generate_dataset(model_name, dataset_name):
+    if model_name == 'c3d':
+        sys.path.append('./datasets/c3d_dataset')
+        test_dataset = get_test_set(dataset_name)
+        sys.path.remove('./datasets/c3d_dataset')
+    return test_dataset
+
+
+# 获取数据集
+def generate_train_dataset(model_name, dataset_name):
+    if model_name == 'c3d':
+        sys.path.append('./datasets/c3d_dataset')
+        from datasets.c3d_dataset.dataset_c3d import get_training_set # 获取数据
+        train_dataset = get_training_set(dataset_name)
+        sys.path.remove('./datasets/c3d_dataset')
+
+    return train_dataset
+
+
+
+# 加载网络模型
+def generate_model(model_name, dataset_name):
+    assert model_name in ['c3d', 'i3d', 'tsm','tsn', 'slowonly']
+    if model_name == 'c3d':
+        if dataset_name == 'k400':
+            from models.c3d.c3d import generate_model_c3d  # 加载模型
+            model = generate_model_c3d(dataset_name)
+            model.eval()
+        else:
+            from models.c3d.c3d import generate_model_c3d  # 加载模型
+            model = generate_model_c3d(dataset_name)
+            model.eval()
+    if model_name=='slowonly':
+        if dataset_name == 'ucf101':
+            cfg = Config.fromfile('configs/recognition/slowonly/slowonly_k400_pretrained_r50_8x4x1_40e_ucf101_rgb.py')
+            checkpoint_path ='configs/checkpoints/slowonly_k400_pretrained_r50_8x4x1_40e_ucf101_rgb_20210630-ee8c850f.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name =='hmdb51':
+            cfg = Config.fromfile('configs/recognition/slowonly/slowonly_k400_pretrained_r50_8x4x1_40e_hmdb51_rgb.py')
+            checkpoint_path ='configs/checkpoints/slowonly_k400_pretrained_r50_8x4x1_40e_hmdb51_rgb_20210630-cee5f725.pth'
+            model= init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name =='k400':
+            cfg = Config.fromfile('configs/recognition/slowonly/slowonly_r50_8x8x1_256e_kinetics400_rgb.py')
+            checkpoint_path ='configs/checkpoints/slowonly_r50_256p_8x8x1_256e_kinetics400_rgb_20200820-75851a7d.pth'
+            model= init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+    if model_name =='tsm':
+        if dataset_name == 'ucf101':
+            cfg = Config.fromfile('configs/recognition/tsm/tsm_k400_pretrained_r50_1x1x16_25e_ucf101_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsm_k400_pretrained_r50_1x1x8_25e_ucf101_rgb_20210630-1fae312b.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name == 'hmdb51':
+            cfg = Config.fromfile('configs/recognition/tsm/tsm_k400_pretrained_r50_1x1x16_25e_hmdb51_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsm_k400_pretrained_r50_1x1x16_25e_hmdb51_rgb_20210630-4785548e.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name == 'k400':
+            cfg = Config.fromfile('configs/recognition/tsm/tsm_r50_1x1x8_100e_kinetics400_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsm_r50_256p_1x1x8_50e_kinetics400_rgb_20200726-020785e2.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+    if model_name =='tsn':
+        if dataset_name == 'ucf101':
+            cfg = Config.fromfile('configs/recognition/tsn/tsn_r50_1x1x3_75e_ucf101_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsn_r50_1x1x3_75e_ucf101_rgb_20201023-d85ab600.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name == 'hmdb51':
+            cfg = Config.fromfile('configs/recognition/tsn/tsn_r50_1x1x8_50e_hmdb51_kinetics400_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsn_r50_1x1x8_50e_hmdb51_kinetics400_rgb_20201123-7f84701b.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+        if dataset_name == 'k400':
+            cfg = Config.fromfile('configs/recognition/tsn/tsn_r50_1x1x3_100e_kinetics400_rgb.py')
+            checkpoint_path = 'configs/checkpoints/tsn_r50_256p_1x1x3_100e_kinetics400_rgb_20200725-22592236.pth'
+            model = init_recognizer(cfg, checkpoint_path, device='cuda:0').eval()
+
+    return model
+
+
+# 根据攻击标识的下坐标id获取对应的攻击图片的label
+def get_attacked_targeted_label(model_name, data_name, attack_id):
+    df = pd.read_csv('./targeted_exp/attacked_samples-{}-{}.csv'.format(model_name, data_name))
+    targeted_label = df[df['attack_id'] == attack_id]['targeted_label'].values.tolist()[0]
+    return targeted_label
+
+
+# 获取用于攻击的测试数据集的index(I3D,LRCN)
+def get_attacked_samples(model, test_data, nums_attack, model_name, data_name):
+    if os.path.exists('./attacked_samples-{}-{}.pkl'.format(model_name, data_name)):
+        # 存在攻击样本
+        with open('./attacked_samples-{}-{}.pkl'.format(model_name, data_name), 'rb') as ipt:
+            attacked_ids = pickle.load(ipt)
+    else:
+        # 随机生辰攻击样本
+        random.seed(1024)
+        idxs = random.sample(range(len(test_data)), len(test_data))  # 随机生成样本index
+        attacked_ids = []   # 攻击样本的index
+        # 保证当前模型可以正确分类此样本
+        for i in idxs:
+            clips, label = test_data[i]
+            video_id = label[0]
+            label = int(label[1])
+            _, pre = classify(model, clips, model_name)
+            if pre != label:
+                pass
+            else:
+                attacked_ids.append(i)
+            if len(attacked_ids) == nums_attack:
+                break
+        with open('./attacked_samples-{}-{}.pkl'.format(model_name, data_name), 'wb') as opt:
+            pickle.dump(attacked_ids, opt)   # 将攻击样本的idx进行保存
+    return attacked_ids
+
+def get_samples(model_name, data_name):
+    if os.path.exists('./untargeted_exp/attacked_samples-{}-{}.pkl'.format(model_name, data_name)):
+        with open('./untargeted_exp/attacked_samples-{}-{}.pkl'.format(model_name, data_name), 'rb') as ipt:
+            attacked_ids = pickle.load(ipt)
+    else:
+        print('No pkl files')
+        return None
+    return attacked_ids
+
+# 获取平均扰动大小
+def pertubation(clean,adv):
+    loss = torch.nn.L1Loss()
+    average_pertubation = loss(clean, adv)
+    return average_pertubation
+
+
+def process_grad(gs, reinforce):
+    if reinforce:
+        g = torch.sign(gs)
+    else:
+        g = torch.tensor(gs)
+    return g
+
+
+
+def speed_up_process(vid,len_limit):
+    actions_t = random.sample(range(0, 16), len_limit)
+    key_t = torch.zeros(vid.size(0))
+    key_t[actions_t] = 1
+    neg_numpy = np.array(actions_t)
+    actions_t = torch.from_numpy(neg_numpy)
+    return actions_t, key_t
